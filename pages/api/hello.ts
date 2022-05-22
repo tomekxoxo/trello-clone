1b4bc28368c884fbed556e812ca71b0d785d122d
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-05-22 16:22:13 +0200

    1- fix api/hello type name

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
+type DataType = {
   name: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<DataType>) {
   res.status(200).json({ name: 'John Doe' });
 }
