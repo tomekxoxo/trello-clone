42e2979892e1f61c26f12bfcab9067c56f02f859
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-05-29 13:26:04 +0200

    6- added typography

---
+++ components/Typography/Typography.style.tsx
@@ -0,0 +1,40 @@
+import { VaritantType, WeightType } from 'Components/Typography/Typography';
+import styled from 'styled-components';
+
+interface IStyledTypographyProps {
+  color: string;
+  variant: VaritantType;
+  weight: WeightType;
+  font: string;
+}
+
+const variantMapping = {
+  h1: {
+    fontSize: '1.8rem',
+    lineHeight: '2.7rem',
+  },
+  h2: {
+    fontSize: '1.6rem',
+    lineHeight: '2.1rem',
+  },
+  h3: {
+    fontSize: '1.4rem',
+    lineHeight: '2.1rem',
+  },
+  h4: {
+    fontSize: '1.2rem',
+    lineHeight: '1.6rem',
+  },
+  h5: {
+    fontSize: '1rem',
+    lineHeight: '1.5rem',
+  },
+};
+
+export const StyledTypography = styled.span<IStyledTypographyProps>`
+  color: ${({ color }) => color};
+  font-family: ${({ font }) => font};
+  font-size: ${({ variant }) => variantMapping[variant].fontSize};
+  font-weight: ${({ weight }) => weight};
+  line-height: ${({ variant }) => variantMapping[variant].lineHeight};
+`;
