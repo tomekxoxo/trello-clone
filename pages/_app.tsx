63f6002397480143dedb98ce96146aca1aa1a5a9
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-05-29 20:05:45 +0200

    6- added layouts component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,11 @@
-import TopBar from 'Components/organisms/TopBar/TopBar';
+import Layout from 'Components/Layout/Layout';
 import type { AppProps } from 'next/app';
-import { createGlobalStyle, css, ThemeProvider } from 'styled-components';
-import { normalize } from 'styled-normalize';
-import theme from 'Utils/theme';
-
-const GlobalStyle = createGlobalStyle`
-${css`
-  ${normalize};
-  html {
-    font-family: 'Noto Sans';
-    font-family: 'Poppins';
-    font-size: 10px;
-  }
-  * {
-    box-sizing: border-box;
-  }
-
-  #__next {
-    bottom: 0;
-    display: flex;
-    flex-direction: column;
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-`}`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <TopBar />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
