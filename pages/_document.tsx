6b1ae3751bc6433a10d77f2c5cc31df55457933d
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-05-29 13:30:36 +0200

    6- document file crossorigin fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ const Document = () => {
     <Html>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
         <link
           href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;600&family=Poppins:wght@400;500;600&display=swap'
           rel='stylesheet'
