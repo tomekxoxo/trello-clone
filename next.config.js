582435293f18b4da96b5c8ef87cb8981cd58add7
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-06-01 15:46:41 +0200

    6- added svgr to project

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
   pageExtensions: ['page.tsx'],
+  experimental: {
+    concurrentFeatures: true,
+  },
 };
 
 module.exports = nextConfig;
