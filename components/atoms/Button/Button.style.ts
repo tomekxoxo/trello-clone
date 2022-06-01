080c03afc8a96f813bb5129afd2c0aea9357e514
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-06-01 17:32:59 +0200

    6- button width adjusted

--- components/atoms/Button/Button.style.ts
+++ components/atoms/Button/Button.style.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 
 interface IStyledButtonProps {
   backgroundColor: string;
+  width: string;
 }
 
 export const SharedButtonStyles = css`
@@ -10,6 +11,7 @@ export const SharedButtonStyles = css`
   border-radius: 8px;
   cursor: pointer;
   display: flex;
+  gap: 0.8rem;
   padding: 9px;
   width: fit-content;
 `;
@@ -17,6 +19,7 @@ export const SharedButtonStyles = css`
 export const StyledButton = styled.button<IStyledButtonProps>`
   ${SharedButtonStyles}
   background-color: ${({ backgroundColor }) => backgroundColor};
+  width: ${({ width }) => width};
 `;
 
 export const StyledButtonIcon = styled.div`
--- components/atoms/Button/Button.tsx
+++ components/atoms/Button/Button.tsx
@@ -3,18 +3,25 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { useTheme } from 'styled-components';
 import { ThemeColorsType } from 'Utils/theme';
 
-interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode;
   color?: ThemeColorsType;
   icon?: JSX.Element;
+  width?: string;
 }
 
-const Button = ({ children, color = 'blue1', icon, ...restProps }: IButtonProps) => {
+const Button = ({
+  children,
+  color = 'blue1',
+  icon,
+  width = 'fit-content',
+  ...restProps
+}: IButtonProps) => {
   const { colors } = useTheme();
   const backgroundColor = colors[color];
 
   return (
-    <StyledButton {...restProps} backgroundColor={backgroundColor}>
+    <StyledButton {...restProps} backgroundColor={backgroundColor} width={width}>
       {icon && <StyledButtonIcon>{icon}</StyledButtonIcon>}
       {children}
     </StyledButton>
