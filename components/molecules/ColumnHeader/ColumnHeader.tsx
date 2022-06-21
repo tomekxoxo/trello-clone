234cacc5386942fadfc421dbe1d3dc45bd83a24b
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-06-21 14:55:19 +0200

    8- dropdown added to columnheader

--- components/molecules/ColumnHeader/ColumnHeader.tsx
+++ components/molecules/ColumnHeader/ColumnHeader.tsx
@@ -2,23 +2,49 @@ import Button from 'Components/atoms/Button/Button';
 import Icon from 'Components/atoms/Icon/Icon';
 import Typography from 'Components/atoms/Typography/Typography';
 import { StyledColumnHeader } from 'Components/molecules/ColumnHeader/ColumnHeader.style';
+import Dropdown from 'Components/molecules/Dropdown/Dropdown';
+import DropdownItem from 'Components/molecules/DropdownItem/DropdownItem';
+import { useState } from 'react';
 
 export interface IColumnHeaderProps {
   status: string;
-  onMenuClick: () => void;
 }
 
-const ColumnHeader = ({ status, onMenuClick }: IColumnHeaderProps) => {
+const ColumnOptions = [
+  {
+    label: 'Rename',
+  },
+  {
+    label: 'Delete this list',
+  },
+];
+
+const ColumnHeader = ({ status }: IColumnHeaderProps) => {
+  const [isOptionsDropdownOpen, setIsOptionsDropdownOpen] = useState(false);
+
+  const handleVisibilityDropdownOpen = () => setIsOptionsDropdownOpen(prevState => !prevState);
+
   return (
     <StyledColumnHeader>
       <Typography color='dark' variant='h3'>
         {status}
       </Typography>
-      <Button
-        color='boardColor'
-        onClick={onMenuClick}
-        icon={<Icon name='ellipsis' color='gray3' />}
-      />
+      <Dropdown
+        attachmentSide='right'
+        closeDropdown={() => setIsOptionsDropdownOpen(false)}
+        isOpen={isOptionsDropdownOpen}
+        anchor={
+          <Button
+            color='boardColor'
+            onClick={handleVisibilityDropdownOpen}
+            icon={<Icon name='ellipsis' color='gray3' />}
+          />
+        }
+      >
+        {ColumnOptions.map((option, index) => (
+          <DropdownItem key={index} label={option.label} labelColor='gray3' />
+        ))}
+      </Dropdown>
     </StyledColumnHeader>
   );
 };
