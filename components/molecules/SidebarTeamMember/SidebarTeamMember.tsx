b7cdb3b6057098a032b9f4eaffdcd63eedb7b058
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-06-25 15:35:51 +0200

    14- added sidebarteammember

---
+++ components/molecules/SidebarTeamMember/SidebarTeamMember.tsx
@@ -0,0 +1,37 @@
+import Button from 'Components/atoms/Button/Button';
+import Typography from 'Components/atoms/Typography/Typography';
+import { StyledSidebarTeamMember } from 'Components/molecules/SidebarTeamMember/SidebarTeamMember.style';
+import User from 'Components/molecules/User/User';
+
+interface ISidebarTeamMemberProps {
+  userImage?: string;
+  userName: string;
+  isAdmin?: boolean;
+  removeMember?: () => void;
+}
+
+const SidebarTeamMember = ({
+  userImage,
+  userName,
+  isAdmin,
+  removeMember,
+}: ISidebarTeamMemberProps) => {
+  return (
+    <StyledSidebarTeamMember>
+      <User name={userName} image={userImage} withName />
+      {isAdmin ? (
+        <Typography color='gray3' variant='h5'>
+          Admin
+        </Typography>
+      ) : (
+        <Button color='red' onClick={removeMember}>
+          <Typography color='white' variant='h5'>
+            Remove
+          </Typography>
+        </Button>
+      )}
+    </StyledSidebarTeamMember>
+  );
+};
+
+export default SidebarTeamMember;
