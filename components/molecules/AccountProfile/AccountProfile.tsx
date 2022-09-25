15ebfa0d2b3c430f664331b706df0761940d7bad
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-09-25 12:54:55 +0200

    22- partial remove of accountPorifle items

--- components/molecules/AccountProfile/AccountProfile.tsx
+++ components/molecules/AccountProfile/AccountProfile.tsx
@@ -1,25 +1,25 @@
-import Icon from 'Components/atoms/Icon/Icon';
-import Popup from 'Components/atoms/Popup/Popup';
+// import Icon from 'Components/atoms/Icon/Icon';
+// import Popup from 'Components/atoms/Popup/Popup';
 import { StyledAccountProfile } from 'Components/molecules/AccountProfile/AccountProfile.style';
 import User from 'Components/molecules/User/User';
-import VisibilityItem from 'Components/molecules/VisibilityItem/VisibilityItem';
-import { useState } from 'react';
+// import VisibilityItem from 'Components/molecules/VisibilityItem/VisibilityItem';
+// import { useState } from 'react';
 
 const AccountProfile = () => {
   const userName = 'Tomasz Kasprowicz';
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  // const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   return (
     <StyledAccountProfile>
       <User name={userName} withName />
-      <Popup
+      {/* <Popup
         anchor={<Icon name='chevron' color='dark' size='16' onClick={() => setIsPopupOpen(true)} />}
         isOpen={isPopupOpen}
         closePopup={() => setIsPopupOpen(false)}
         attachmentSide='right'
       >
         <VisibilityItem label='Logout' labelColor='gray3' />
-      </Popup>
+      </Popup> */}
     </StyledAccountProfile>
   );
 };
