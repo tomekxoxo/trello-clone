586800aede10847d1be8c6d053627dc663fb6eb8
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-10-02 14:34:29 +0200

    26- index file for resolvers

---
+++ graphql/resolvers/index.ts
@@ -0,0 +1,9 @@
+import { mutation } from 'graphql/resolvers/mutation';
+import { query } from 'graphql/resolvers/query';
+
+const resolvers = {
+  Mutation: mutation,
+  Query: query,
+};
+
+export default resolvers;
