36d1c904c194edfafdd28f9740657e803bf17074
tomaszKasprowicz <tomaszkasprowicz24@gmail.com>
2022-10-03 11:23:45 +0200

    26-auth utils created

---
+++ graphql/auth.ts
@@ -0,0 +1,28 @@
+import { AuthenticationError } from 'apollo-server-micro';
+import type { MicroRequest } from 'apollo-server-micro/dist/types';
+import jwt from 'jsonwebtoken';
+
+export const JWT_SECRET = process.env.JWT_SECRET as string;
+
+function getTokenPayload(token: string) {
+  return jwt.verify(token, JWT_SECRET, error => {
+    if (error) {
+      throw new AuthenticationError('Token expired, please login again');
+    }
+  });
+}
+
+export function authenticate(req: MicroRequest) {
+  if (req) {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.replace('Bearer ', '');
+      if (!token) {
+        throw new AuthenticationError('No token found');
+      }
+      return getTokenPayload(token);
+    }
+  }
+
+  throw new AuthenticationError('Not authenticated');
+}
