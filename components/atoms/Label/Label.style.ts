ef7567f270cd108f85ea2d0b335ac8c5917eaaf4
Tomasz Kasprowicz <tomaszkasprowicz24@gmail.com>
2023-06-13 10:38:39 +0200

    label component changed markup

--- components/atoms/Label/Label.style.ts
+++ components/atoms/Label/Label.style.ts
@@ -5,7 +5,7 @@ interface StyledLabelProps {
   color: ColorResult['hex'];
 }
 
-export const StyledLabel = styled.label<StyledLabelProps>`
+export const StyledLabel = styled.div<StyledLabelProps>`
   background-color: ${({ color }) => color};
   border-radius: 0.8rem;
   padding: 0.2rem 0.9rem;
