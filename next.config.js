70104d26520dd8357200c95321d172aaf49e325c
Tomasz Kasprowicz <tomaszkasprowicz24@gmail.com>
2023-06-14 20:45:01 +0200

    nextconfig updated

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -26,14 +9,11 @@ const nextConfig = {
 
     return config;
   },
-  reactStrictMode: false,
+  reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
   pageExtensions: ['page.tsx', 'page.ts', 'api.ts'],
-  experimental: {
-    concurrentFeatures: true,
-  },
   images: {
     domains: ['images.pexels.com', 'lh3.googleusercontent.com'],
   },
